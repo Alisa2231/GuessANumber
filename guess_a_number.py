8d78b667847b9877e43ff100e0dd4d6362e6a11d
Asya Nikolova <asya22@abv.bg>
2025-05-29 10:32:42 +0300

    added option to restart the game

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,25 +1,35 @@
 import random
 
 print("Let's play the game 'Guess A Number'!")
-random_number = random.randint(1, 100)
-print("The computer has generated a random number between 1 and 100 inclusive, you have to guess it!")
+play = "y"
+while play == "y":
+    random_number = random.randint(1, 100)
+    print("The computer has generated a random number between 1 and 100 inclusive, you have to guess it!")
 
-player_guess = input("Guess the number: ")
-while not player_guess.isdigit():
-    print("Invalid input, the game works with numbers only.")
     player_guess = input("Guess the number: ")
-player_guess = int(player_guess)
-attempts = 1
-while player_guess != random_number:
-    if player_guess < random_number:
-        print(f"Your guess {player_guess} is smaller than the number.")
-    else:
-        print(f"Your guess {player_guess} is bigger than the number.")
-    player_guess = input("Guess again: ")
     while not player_guess.isdigit():
         print("Invalid input, the game works with numbers only.")
-        player_guess = input("Guess again: ")
+        player_guess = input("Guess the number: ")
     player_guess = int(player_guess)
-    attempts += 1
+    attempts = 1
+    while player_guess != random_number:
+        if player_guess < random_number:
+            print(f"Your guess {player_guess} is smaller than the number.")
+        else:
+            print(f"Your guess {player_guess} is bigger than the number.")
+        player_guess = input("Guess again: ")
+        while not player_guess.isdigit():
+            print("Invalid input, the game works with numbers only.")
+            player_guess = input("Guess again: ")
+        player_guess = int(player_guess)
+        attempts += 1
 
-print(f"Congratulations, you guessed the number {random_number} in {attempts} attempts!")
+    print(f"Congratulations, you guessed the number {random_number} in {attempts} attempts!")
+    print()
+    play = input("Wanna play again? y/n ")
+    while play not in ["y", "n"]:
+        print("Invalid input, please type 'y' or 'n'.")
+        play = input("Wanna play again? y/n ")
+    if play == 'n':
+        print("Thank you for playing!")
+        break
